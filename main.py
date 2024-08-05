a089256dd307059ed2b4d2786dba8131b82420df
Hyunju Jennie Lee <hj.jennie.lee@gmail.com>
2024-08-04 20:51:01 -0600

    retrieve and scrap data from the website using beautifulSoup

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ browser = p.chromium.launch(headless=False)
 
 page = browser.new_page()
 
-page.goto("https://www.wanted.co.kr/jobsfeed")
-time.sleep(5)
+page.goto("https://www.wanted.co.kr/search?query=flutter&tab=position")
 
-page.click("button.Aside_searchButton__rajGo")
-time.sleep(5)
+#page.goto("https://www.wanted.co.kr/jobsfeed")
+# time.sleep(5)
 
-page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
-time.sleep(5)
+# page.click("button.Aside_searchButton__rajGo")
+# time.sleep(5)
 
-page.keyboard.down("Enter")
-time.sleep(5)
+# page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+# time.sleep(5)
 
-page.click("a#search_tab_position")
-time.sleep(5)
+# page.keyboard.down("Enter")
+# time.sleep(5)
+
+# page.click("a#search_tab_position")
+# time.sleep(5)
+
+jobs_db = []
 
 for x in range(5):
     time.sleep(5)
@@ -31,4 +35,22 @@ content = page.content()
 p.stop()
 
 soup = BeautifulSoup(content, "html.parser")
-print(soup)
+
+jobs = soup.find_all("div", class_="JobCard_container__REty8")
+
+for job in jobs:
+    link = f"https://www.wanted.co.kr/{job.find('a')['href']}"
+    title = job.find("strong", class_="JobCard_title__HBpZf").text
+    company_name = job.find("span", class_="JobCard_companyName__N1YrF").text
+    
+    job = {
+        "link" : link,
+        "title": title,
+        "company_name" : company_name
+    }
+    jobs_db.append(job)
+    
+print(jobs_db)
+print(len(jobs_db))
+
+
